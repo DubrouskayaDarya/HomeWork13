859e07f3a1ab0913df3fe4a9252f1d79114878da
Дарья Дубровская <>
2022-01-30 17:53:17 +0300

    Task 13: Added View Controllers and elements

---
+++ HomeWork13/HomeWork13/Controllers/SignUpControllers/CodeVerifVC.swift
@@ -0,0 +1,29 @@
+//
+//  CodeVerifVC.swift
+//  HomeWork13
+//
+//  Created by Дарья Дубровская on 29.01.22.
+//
+
+import UIKit
+
+class CodeVerifVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
