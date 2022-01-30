1cf3e9eaf62c06857dcfcd9393cc659af8e2d2dc
Дарья Дубровская <>
2022-01-31 01:44:40 +0300

    Task 13: Added actions and validations on SignInVC and CreateAccountVC:

--- HomeWork13/HomeWork13/Controllers/SignInControllers/SignInVC.swift
+++ HomeWork13/HomeWork13/Controllers/SignInControllers/SignInVC.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var labelUserNotFound: UILabel!
+    @IBOutlet weak var signInButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
         // Do any additional setup after loading the view.
     }
+    @IBAction func unwindToSignInVC(_ unwindSegue: UIStoryboardSegue) {
+    }
 
-
+    func setupUI() {
+        signInButton.layer.cornerRadius = 8.0
+    }
 }
 
